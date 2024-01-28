6afa473558d819569040ab03bd67df3993a8d0bd
satishmarutham <satishlakshmiudemy@gmail.com>
2024-01-28 20:38:21 +0530

    Pages Set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
-
+import {Routes,Route,BrowserRouter} from 'react-router-dom'
+import Home  from "./pages/Home";
+import About from "./pages/About";
+import SignIn  from "./pages/SignIn";
+import SignOut from "./pages/SignOut";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 export default function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>} />
+      <Route path='/signIn' element={<SignIn/>}/>
+      <Route path='/signOut' element={<SignOut/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
