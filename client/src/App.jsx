1178b605a8e4680f5df5402914597cd8b5351798
satishmarutham <satishlakshmiudemy@gmail.com>
2024-01-29 10:21:36 +0530

    Header Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
 import React from 'react'
-import {Routes,Route,BrowserRouter} from 'react-router-dom'
-import Home  from "./pages/Home";
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from "./pages/Home";
 import About from "./pages/About";
-import SignIn  from "./pages/SignIn";
+import SignIn from "./pages/SignIn";
 import SignOut from "./pages/SignOut";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
+import Header from './components/Header';
 export default function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>} />
-      <Route path='/signIn' element={<SignIn/>}/>
-      <Route path='/signOut' element={<SignOut/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-      <Route path='/projects' element={<Projects/>}/>
-    </Routes>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signIn' element={<SignIn />} />
+        <Route path='/signOut' element={<SignOut />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
     </BrowserRouter>
   )
 }
