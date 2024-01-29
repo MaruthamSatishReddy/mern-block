9f1e5c3cfcdd0f64905c32c219ee19f03b92757f
satishmarutham <satishlakshmiudemy@gmail.com>
2024-01-29 13:24:12 +0530

    Added Server and MongoDb DataBase

--- api/index.js
+++ api/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
 
-
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Mongo Db is Connected");
+}).catch(error => {
+    console.error("Getting Error While Connecting MongoDB:" + error);
+});
 const app = express();
+
 app.listen(3000, () => {
     console.log("Server Is Running::3000");
 }) 
